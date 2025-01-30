896a17a671cfd9a1209243c2250a4178867f51df
root <root@DES-ANATOLII.>
2025-01-30 21:52:52 +0300

    docker-compos

--- my-docker-app/app.py
+++ my-docker-app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 @app.route('/')
 def hello():
-    return "Hello, Docker!"
+    return "Hello, Docker Compose!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
